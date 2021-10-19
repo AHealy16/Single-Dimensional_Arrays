aa0707efd479f5390ad6624d979f0f281d7b811d
AHealy16 <AH211841@reigate.ac.uk>
2021-10-19 14:38:08 +0100

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,24 @@
+package com.company;
+import java.util.Random;
+public class Main {
+
+   public static void main(String[] args) {
+//
+//        Random random = new Random();
+//        int[] favouriteNumbers = new int[10];
+//
+//        for (int i = 0; i < favouriteNumbers.length; i++) {
+//            favouriteNumbers[i]
+//
+//
+//        }
+
+    Random random = new Random();
+    char[] favouriteChars = {'a', 'z', 'w', 'c'};
+
+    for (char favouriteChar : favouriteChars){
+
+        System.out.println(favouriteChar);
+    }
+    }
+}
